0c0a5f5a286d81e1c53431e72d253bda444838f2
ayush24-eth <ayushtmsl611@gmail.com>
2023-10-14 01:53:53 +0530

    updated contract

--- contracts/liquidity_pool.sol
+++ contracts/liquidity_pool.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
@@ -13,15 +13,15 @@ contract LiquidityPool {
 
     event LiquidityAdded(address indexed provider, uint256 amountA, uint256 amountB);
     event TokensSwapped(address indexed user, uint256 amountAIn, uint256 amountBOut);
-    event CheckBalance(string text, uint amount);
+    event CheckBalance(uint amount);
 
-    // Uncomment the constructor and complete argument details in scripts/deploy.ts to deploy the contract with arguments
+    //complete argument details in scripts/deploy.ts to deploy the contract with arguments
 
-    // constructor(address _tokenA, address _tokenB) {
-    //     tokenA = IERC20(_tokenA);
-    //     tokenB = IERC20(_tokenB);
-    //     owner = msg.sender;
-    // }
+    constructor(address _tokenA, address _tokenB) {
+        tokenA = IERC20(_tokenA);
+        tokenB = IERC20(_tokenB);
+        owner = msg.sender;
+    }
 
     // Add liquidity to the pool
     function addLiquidity(uint256 amountA, uint256 amountB) external {
@@ -53,11 +53,9 @@ contract LiquidityPool {
         emit TokensSwapped(msg.sender, amountAIn, amountBOut);
     }
     
-    function getBalance(address user_account) external returns (uint){
-       string memory data = "User Balance is : ";
+    function getBalance(address user_account) external returns (uint) {
        uint user_bal = user_account.balance;
-       emit CheckBalance(data, user_bal );
+       emit CheckBalance(user_bal);
        return (user_bal);
-
     }
 }
